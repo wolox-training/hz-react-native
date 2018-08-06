ac85603fa530d073561f9df4a820bbab031335e5
Henry Zarza <henryacz7@gmail.com>
2018-08-06 16:31:27 -0500

    Starter files and spread operator test 1 passed

---
+++ react/fingerexercises/01 - spread/index.js
@@ -0,0 +1,13 @@
+import isArray from './utils';
+
+export function min(param) {
+  if (isArray(param)) {
+    // do something
+    return null; //Math.min();
+  }
+  return param;
+}
+
+export function copy() {
+
+}
