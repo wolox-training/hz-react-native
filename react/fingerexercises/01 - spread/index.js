22a17f77c53cc533e375f61eac7520936271018d
Henry Zarza <henryacz7@gmail.com>
2018-08-06 17:23:43 -0500

    Spread operator test 4

--- react/fingerexercises/01 - spread/index.js
+++ react/fingerexercises/01 - spread/index.js
@@ -1,7 +1,9 @@
 import isArray from './utils';
 
 export function min(...params) {
-  return isArray(params[0]) ? Math.min(...params[0]) : Math.min(...params);
+  if (params.length > 0)
+    return isArray(params[0]) ? Math.min(...params[0]) : Math.min(...params);
+  return undefined;
 }
 
 export function copy() {
