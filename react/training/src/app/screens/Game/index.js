6eedb8e76dd99550179bd63e2c1a0317c6f02f39
Henry Zarza <henryacz7@gmail.com>
2018-08-10 16:34:47 -0500

    Implementing time travel

--- react/training/src/app/screens/Game/index.js
+++ react/training/src/app/screens/Game/index.js
@@ -17,11 +17,12 @@ function calculateWinner(squares) {
 export default class Game extends Component {
   state = {
     history: [{ squares: Array(9).fill(null) }],
+    stepNumber: 0,
     xIsNext: true
   };
 
   handleClick = i => {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -30,15 +31,30 @@ export default class Game extends Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{ squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   };
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start';
+      return (
+        <li key={`autoKey-${move + 1}`}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
     let status;
     if (winner) {
       status = `Winner: ${winner}`;
@@ -53,7 +69,7 @@ export default class Game extends Component {
         </div>
         <div className={style.gameInfo}>
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
