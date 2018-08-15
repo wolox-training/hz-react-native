0705b8169095661c8a834cfcaebfe4586169a2fb
Henry Zarza <henryacz7@gmail.com>
2018-08-15 15:06:22 -0500

    Redux and redux form

--- react/training/src/index.js
+++ react/training/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import RouteWrapper from '~components/RouteWrapper'; // eslint-disable-line import/first;
+import RouteWrapper from '~components/RouteWrapper';
+
+import store from '~redux/store';
 
 import './scss/index.scss';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<RouteWrapper />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <RouteWrapper />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
